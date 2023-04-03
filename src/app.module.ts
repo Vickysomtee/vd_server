f8ab0268fe2a49e1a57ae6b492211e2286db57ff
vickysomtee <vickysomtee@gmail.com>
2023-04-03 23:10:18 +0100

    feat: add migrations
    Signed-off-by: vickysomtee <vickysomtee@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
-import { typeOrmAsyncConfig } from './database/database.provider';
-
+import { typeOrmConfig } from './database/database.provider';
 import { AuthModule } from './auth/auth.module';
 import { SeminariansModule } from './seminarians/seminarians.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    TypeOrmModule.forRoot(typeOrmConfig),
     AuthModule,
     SeminariansModule,
   ],
--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,28 +1,27 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import {
-  TypeOrmModuleAsyncOptions,
-  TypeOrmModuleOptions,
-} from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
-export default class TypeOrmConfig {
-  static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
-    return {
-      type: 'mysql',
-      host: configService.get('DB_HOST'),
-      port: configService.get('DB_PORT'),
-      username: configService.get('DB_USERNAME'),
-      password: configService.get('DB_PASSWORD'),
-      database: configService.get('DB_NAME'),
-      entities: ['dist/**/*.entity.js'],
-      synchronize: true,
-    };
-  }
+import { DataSource, DataSourceOptions } from 'typeorm';
+import {config} from 'dotenv'
+
+import { Seminarians } from 'src/entities/seminarian.entities';
+
+config()
+
+const configService = new ConfigService()
+
+export const typeOrmConfig: DataSourceOptions = {
+  type: 'mysql',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  entities: [Seminarians,],
+  migrations: ['dist/database/migrations/*.js'],
+  synchronize: true,
 }
 
-export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
-  imports: [ConfigModule],
-  useFactory: async (
-    configService: ConfigService,
-  ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
-  inject: [ConfigService],
-};
+const dataSource = new DataSource(typeOrmConfig)
+
+export default dataSource;
+
