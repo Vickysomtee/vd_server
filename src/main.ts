8a632f8ec76873f5e870c8869ec401f4d584156f
vickysomtee <vickysomtee@gmail.com>
2023-04-06 18:09:45 +0100

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder().setTitle('Vocational Director Portal')
-  .setDescription("API for the VD Portal")
-  .setVersion('v1')
-  .addTag('books')
-  .build();
-
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
+  app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(+process.env.PORT);
 }
