e45259e86da63176d62838bae71a2cbfb9f2fa92
vickysomtee <vickysomtee@gmail.com>
2023-05-11 23:50:50 +0100

    fix: seminarian test case

--- src/seminarians/tests/seminarians.service.spec.ts
+++ src/seminarians/tests/seminarians.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { SeminariansService } from './seminarians.service';
+import { SeminarianService } from '../seminarians.service';
 
 describe('SeminariansService', () => {
-  let service: SeminariansService;
+  let service: SeminarianService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SeminariansService],
+      providers: [SeminarianService],
     }).compile();
 
-    service = module.get<SeminariansService>(SeminariansService);
+    service = module.get<SeminarianService>(SeminarianService);
   });
 
   it('should be defined', () => {
