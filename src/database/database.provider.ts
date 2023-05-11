1486303292482ac9295fa7d528a88d23dfc2c6a1
vickysomtee <vickysomtee@gmail.com>
2023-05-11 23:51:19 +0100

    chore: revert deployment changes

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -10,13 +10,13 @@ config()
 const configService = new ConfigService()
 
 export const typeOrmConfig: DataSourceOptions = {
-  type: 'postgres',
+  type: 'mysql',
   host: configService.get('DB_HOST'),
   port: configService.get('DB_PORT'),
   username: configService.get('DB_USERNAME'),
   password: configService.get('DB_PASSWORD'),
   database: configService.get('DB_NAME'),
-  ssl: true,
+  // ssl: true,
   entities: [Seminarians,],
   migrations: ['dist/database/migrations/*.js'],
   synchronize: true,
