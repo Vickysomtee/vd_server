72397a5bf86bb84ffc67b18501ce157abac7f96e
vickysomtee <vickysomtee@gmail.com>
2023-06-23 14:13:42 +0100

    fix: jwt stratetegy

--- src/modules/auth/strategy/access_token.strategy.ts
+++ src/modules/auth/strategy/access_token.strategy.ts
@@ -15,7 +15,7 @@ export class AccessTokenStrategy extends PassportStrategy(Strategy) {
       ) {
         super({
           jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-          secretOrKey: configService.get<string>('JWT_SECRET'),
+          secretOrKey: 'gggggggggjguf',
         });
       }
 
