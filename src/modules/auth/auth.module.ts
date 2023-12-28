f9bc577321c6ea6a9bae5aafb06a3aa8c6790fc0
vickysomtee <vickysomtee@gmail.com>
2023-12-28 07:17:17 +0100

    remove jwt expiry

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -18,7 +18,7 @@ import { AuthController } from './auth.controller';
       useFactory: (configService: ConfigService) => ({
         secret: configService.get('JWT_SECRET'),
         signOptions: {
-          expiresIn: configService.get('JWT_EXPIRES'),
+          // expiresIn: configService.get('JWT_EXPIRES'),
         },
       }),
     }),
